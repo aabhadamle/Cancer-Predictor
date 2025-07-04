5c4ce8a90f7f95f0088488c7503de36cd4c1e2f4
aabha15 <aabhadamle15@gmail.com>
2025-07-04 11:32:04 +0530

    Update

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def predict():
         return render_template("result.html", prediction="Invalid input. Please enter numeric values.")
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port)
